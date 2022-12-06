a39e6a36bda49c32e33434facc221d98cd9d2149
AlleBedo <alle.bedo@gmail.com>
2022-12-06 23:11:54 +0100

    day 6 easy peasy lemon squeezy

---
+++ 2022/Bedo/AoC_6_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Insert a description here.
+
+    Bugs: none known
+ 
+
+    Author: Alessandro Bedini alle.bedo@gmail.com
+
+    Date Created: 05/12/2022
+"""
+ 
+
+FILE_PATH = "2022/Bedo/resources/AoC_6.txt"
+
+if __name__ == "__main__":
+
+
+    with open(FILE_PATH, "r") as file:
+        for line in file:
+            index = 4
+            while index < len(line):
+                if len(set(line[index - 4:index])) == 4:
+                    print(index)
+                    break
+                index += 1
